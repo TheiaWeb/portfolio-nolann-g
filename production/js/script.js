deed4a082f942274be4fac6d15f75530baea1142
Haxxxxxx <vinc.charles0@gmail.com>
2023-11-17 17:45:15 +0100

    update css

---
+++ production/js/script.js
@@ -0,0 +1,6 @@
+const content = document.getElementById("testslide");
+const btnleft = document.getElementById("left");
+const btncenter = document.getElementById("left");
+const btnrihgt = document.getElementById("left");
+
+document.addEventListener("click", btncenter)
