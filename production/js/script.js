5b9402c3eb2d7697cc7c6044788724032f544b24
Haxxxxxx <vinc.charles0@gmail.com>
2023-11-17 19:33:34 +0100

    selected + hover

--- production/js/script.js
+++ production/js/script.js
@@ -38,3 +38,16 @@ document.addEventListener("DOMContentLoaded", function () {
     showSection(entreprisesSection);
   });
 });
+const buttons = document.querySelectorAll('.nav-button');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    // Remove the 'active' class from all buttons
+    buttons.forEach((btn) => {
+      btn.classList.remove('active');
+    });
+
+    // Add the 'active' class to the clicked button
+    button.classList.add('active');
+  });
+});
